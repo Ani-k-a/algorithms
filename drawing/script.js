abbb0fff32131974a11da37a66ce7af632585a33
Hanna.Lytvynenko <ani-k-a@ukr.net>
2023-04-12 17:50:10 +0200

    hw 27/03

--- drawing/script.js
+++
@@ -1,7 +0,0 @@
-function draw(x, y) {
-    if (pic[x][y] === 0) {
-        pic[x][y] = 1;
-        draw(x-1, y);
-        draw
-    }
-}
--- hanoi/script.js
+++
@@ -1,8 +0,0 @@
-function hanoi (q, from, to, buf){
-    if(q!==0){
-hanoi(q-1, from, buf, to);
-console.log(` ${from} >>> ${to}`);
-hanoi(q-1, buf, to, from);
-    }
-}
-hanoi(3, 1, 3, 2);
